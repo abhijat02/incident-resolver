6f0436e0d6d5678c29e14b4fbb736ba2e2214c8b
Abhijat <ironsuck69@gmail.com>
2026-02-24 14:20:44 +0530

    started

---
+++ src/main/java/com/demo/ai_incident_resolver/AiIncidentResolverApplication.java
@@ -0,0 +1,13 @@
+package com.demo.ai_incident_resolver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiIncidentResolverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiIncidentResolverApplication.class, args);
+	}
+
+}
